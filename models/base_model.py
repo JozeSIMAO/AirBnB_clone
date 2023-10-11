13439c61583aee044b7d520a40618a9df4bd8f51
JozeSIMAO <jozesimao10@gmail.com>
2023-10-11 21:46:35 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -19,11 +19,19 @@ class BaseModel:
     - to_dict(): Converts the object to a dictionary with specific formatting.
     - __str__(): Returns a str representation of the obj in a specific format.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel object with unique ID and timestamps."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        timeformat = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        value = datetime.strptime(value, timeformat)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
