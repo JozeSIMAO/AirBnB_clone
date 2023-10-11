7809c411d6b16776de0f84cdc16bd4028f3fa0d8
JozeSIMAO <jozesimao10@gmail.com>
2023-10-11 15:09:19 +0200

    Edited the BaseModel Class

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""Defines a class BaseModel"""
 
-#Inorder to use the uuid and dattime to be able to assign a UUID and to get timestamps
+
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    # No predefine class attributes (ASSIGNMENTS)
     """
     BaseModel represents a basic model with common attributes and methods.
     Public instance attributes:
@@ -16,42 +17,40 @@ class BaseModel:
     Public instance methods:
     - save(): Updates the 'updated_at' timestamp to the current time.
     - to_dict(): Converts the object to a dictionary with specific formatting.
-    - __str__(): Returns a string representation of the object in a specific format.
+    - __str__(): Returns a str representation of the obj in a specific format.
     """
     def __init__(self):
-        # Instant Attribute (FUNCTION): Generate a unique ID using uuid.uuid4() and convert it to a string
         """Initialize a new BaseModel object with unique ID and timestamps."""
         self.id = str(uuid.uuid4())
-
-        # Instant Attribute (FUNCTION): Sets the creation date and time (created_at) to the current datetime
         self.created_at = datetime.now()
-
-        # Instant Attribute (FUNCTION): Sets the initial updated_at to be the same as created_at
         self.updated_at = self.created_at
 
     def __str__(self):
         """
-        Return a string representation of the object.
+        Returns a string representation ofo the object
 
         Returns:
-            str: A string representation of the object in a specific format.
+            str: A string representation of the object in a specific format
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__) #str.format used #check if it prints when building console.py
+        # str.format used #check if it prints when building console.py
+        return f"{self.__class__}.{__name__}, {self.id}, {self.__dict__}"
 
     def save(self):
-        # This method will Update the updated_at attribute with the current datetime
         """Update the 'updated_at' timestamp to the current time."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-         """
+        """
         Convert the object to a dictionary.
 
         Returns:
-            dict: A dictionary representation of the object with specific formatting.
+            dict: A dict representation of the object with specific formatting.
         """
-        obj_dict = self.__dict__  # Get all the attributes and their values
-        obj_dict['__class__'] = self.__class__.__name__  # Add the class name
-        obj_dict['created_at'] = self.created_at.isoformat()  # Converting it into ISO format
-        obj_dict['updated_at'] = self.updated_at.isoformat()  # Converting it into ISO format
+        # Get all the attributes and their values
+        obj_dict = self.__dict__.copy()
+        # Add the class name
+        obj_dict['__class__'] = self.__class__.__name__
+        # Converting it into ISO format
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
