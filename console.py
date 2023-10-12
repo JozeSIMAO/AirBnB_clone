8708e1d0cf68954e16d2bc92855750e8f451fc56
JozeSIMAO <jozesimao10@gmail.com>
2023-10-12 18:52:29 +0200

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Defines a class HBNBCommand, entry point of the command interpreter"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Represents the the HBNBCommand class"""
+    prompt = '(hbnb) '
+    
+    def do_quit(self, arg):
+        """Exits the program if (quit) is input"""
+        return True
+    
+    def do_EOF(self, arg):
+        """Exits the program if (EOF) is input"""
+        return True
+    
+    def emptyline(self):
+        """Does nothing"""
+        pass
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
