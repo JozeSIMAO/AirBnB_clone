6662327720e45d1d91ede72dcf1032e897eaac70
JozeSIMAO <jozesimao10@gmail.com>
2023-10-14 09:03:06 +0200

    Do_destroy fix

--- console.py
+++ console.py
@@ -29,7 +29,7 @@ class HBNBCommand(cmd.Cmd):
         elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
-            file_storage = FileStorage() #creates an object that represents an instance of FileStorage class
+            file_storage = FileStorage()
             new_instance = BaseModel() #creating a new class object
             print(new_instance.id) #Prints id data of object to console.py
             file_storage.new(new_instance) #add the new instance to the storage system
@@ -61,74 +61,68 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Deletes an instance based on the class name and id"""
         if not arg:
-         print("**class name missing *Z*")
-        return
-
-    
-        args = arg.split() #Splits the argument into a list of words
-        class_name = args[0]
+            print("**class name missing **")
+            return
         
-        if len(args) < 2: # This checks if the argument contains a class name and an instance id
-            print("** instance id missing **")
-        else:
-            instance_id = args[1]
-            instances = storage.all()
-            key = f"{class_name}.{instance_id}"
-
-        # Check if the instance exists and delete it if found
-        if key in instances:
-            del instances[key] #If Key is found, Delete
-            storage.save() #Save progress
-        else:
-            print("** no instance found **") #If the class name and id does not exist
-
-def do_all(self, arg):
-    """Prints all string representations of instances"""
-    args = arg.split() #Splits the argument into a list of words
-
-  
-    if not arg:  # This checks if the argument is empty, and if so, print all instances
-        print([str(instance) for instance in storage.all().values()]) #We use str() to turn the ibject instance into a printable string
-    elif args:
+        args = arg.split()
         class_name = args[0]
-
-      
-        if class_name in storage.all():  # This check if the class name exists in the storage
-
-           
-            print([str(instance) for instance in storage.all().values() if instance.__class__.__name__ == class_name]) # Now we are filterering instances by class name and print them
-        else: #If class name and ID does not exist
-            print("** class doesn't exist **")
-
-def do_update(self, arg):
-    """Updates an instance's attribute"""
-    if not arg: #If instance is not in arg
-        print("** class name missing **")
-    else:
-        args = arg.split() #Splits the argument into a list of words
-        class_name = args[0] #Give first word in args variable name class_name
-
-    
-        if len(args) < 2:    #This checks if the argument contains a class name and an instance id. Use Len() to filter condition
+        file_storage = FileStorage()
+        
+        if len(args) < 2:
             print("** instance id missing **")
-        elif f"{class_name}.{instance_id}" not in storage.all():
-            print("** no instance found **")
-        elif len(args) < 3:
-            print("** attribute name missing **")
-        elif len(args) < 4:
-            print("** value missing **")
         else:
             instance_id = args[1]
-            attr_name = args[2]
-            attr_value = args[3]
-
-            
-            instance = storage.all().get(f"{class_name}.{instance_id}")#We can now get the instance from storage
-
-            if instance:#Finally, we can set the attribute value and save the instance
+            instances = file_storage.all()
+            key = f"{class_name}.{instance_id}"
+        
+        if key in instances:
+            del instances[key]
+            file_storage.save()
+        else:
+            print("** no instance found **")
+    
+    def do_all(self, arg):
+        """Prints all string representations of instances"""
+        file_storage = FileStorage()
+        instances = file_storage.all()
+        args = arg.split() #Splits the argument into a list of words
+        if not arg:  # This checks if the argument is empty, and if so, print all instances
+            print([str(instance) for instance in file_storage.all().values()]) #We use str() to turn the ibject instance into a printable string
+        elif args:
+            class_name = args[0]
+            if class_name not in instances: #If class name and ID does not exist
+                print("** class doesn't exist **")
+            elif class_name in instances:  # This check if the class name exists in the storage
+                for instance in instances.values():
+                    print(str(instance))
                 
-                setattr(instance, attr_name, attr_value)
-                instance.save()
+                
+    def do_update(self, arg):
+        """Updates an instance's attribute"""
+        file_storage = FileStorage()
+        
+        if not arg: #If instance is not in arg
+            print("** class name missing **")
+        else:
+            args = arg.split() #Splits the argument into a list of words
+            class_name = args[0] #Give first word in args variable name class_name
+            if len(args) < 2:    #This checks if the argument contains a class name and an instance id. Use Len() to filter condition
+                print("** instance id missing **")
+            elif f"{class_name}.{instance_id}" not in file_storage.all():
+                print("** no instance found **")
+            elif len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            else:
+                instance_id = args[1]
+                attr_name = args[2]
+                attr_value = args[3]
+                instance = file_storage.all().get(f"{class_name}.{instance_id}")#We can now get the instance from storage
+                
+                if instance:#Finally, we can set the attribute value and save the instance
+                    setattr(instance, attr_name, attr_value)
+                    instance.save()
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a class BaseModel"""
 
-
+import models
 import uuid
 from datetime import datetime
 
@@ -21,6 +21,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel object with unique ID and timestamps."""
         timeformat = '%Y-%m-%dT%H:%M:%S.%f'  #Much better way of writing this!
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -28,9 +31,7 @@ class BaseModel:
                         value = datetime.strptime(value, timeformat)
                     setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -40,11 +41,13 @@ class BaseModel:
             str: A string representation of the object in a specific format
         """
         # str.format used #check if it prints when building console.py
-        return f"{self.__class__}.{__name__}, {self.id}, {self.__dict__}"
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
         """Update the 'updated_at' timestamp to the current time."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
