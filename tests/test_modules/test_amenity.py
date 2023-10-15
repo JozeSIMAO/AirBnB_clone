fa679ec0df66c327aa1a9182e997c3900791a8de
Believe Liphadzi <122865634+pbliphadzi@users.noreply.github.com>
2023-10-15 17:09:33 +0000

    Unittests - incomplete - Believe

---
+++ tests/test_modules/test_amenity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import unittest
+from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+
+    def test_instance_creation(self):
+        amenity = Amenity()
+        self.assertIsInstance(amenity, Amenity)
+
+    def test_attributes(self):
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+        amenity.name = "Swimming Pool"
+        self.assertEqual(amenity.name, "Swimming Pool")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_modules/test_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import unittest
+from models.city import City
+
+class TestCity(unittest.TestCase):
+
+    def test_instance_creation(self):
+        city = City()
+        self.assertIsInstance(city, City)
+
+    def test_attributes(self):
+        city = City()
+        self.assertTrue(hasattr(city, 'id'))
+        self.assertTrue(hasattr(city, 'created_at'))
+        self.assertTrue(hasattr(city, 'updated_at'))
+        self.assertTrue(hasattr(city, 'state_id'))
+        self.assertTrue(hasattr(city, 'name'))
+
+    def test_str_representation(self):
+        city = City()
+        expected_str = "[City] ({}) {}".format(city.id, city.__dict__)
+        self.assertEqual(str(city), expected_str)
+
+if __name__ == '__main__':
+    unittest.main()
